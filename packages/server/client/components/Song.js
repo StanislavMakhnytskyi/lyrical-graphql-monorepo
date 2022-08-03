cbf5222dfbebef70a604d10349584fe2af464fe3
StanislavMakhnytskyi <makhnitskiy.stanislav@gmail.com>
2022-08-03 22:35:25 +0400

    tech(server): remove extra code

--- packages/server/client/components/Song.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const Song = (song) => {
-  return <li>{song.toString()}</li>;
-};
--- packages/server/index.js
+++ packages/server/index.js
@@ -1,4 +1,4 @@
-const app = require('./server/server');
+const app = require('./src/server');
 
 app.listen(4000, () => {
   console.log('Listening');
