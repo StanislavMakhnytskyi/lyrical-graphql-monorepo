2f859a7e57b8196b976db4bd8f626ec029b1e61e
StanislavMakhnytskyi <makhnitskiy.stanislav@gmail.com>
2022-08-17 13:06:17 +0400

    feature(web-ui): add back button to song details

--- packages/web-ui/features/songs/song-details.tsx
+++ packages/web-ui/features/songs/song-details.tsx
@@ -1,15 +1,27 @@
 import React, { FC } from 'react';
 import Grid from '@mui/material/Grid';
 import { ISong } from '../../types/types';
+import Button from '@mui/material/Button';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { useRouter } from 'next/router';
+import Typography from '@mui/material/Typography';
 
 export interface Props {
   song: ISong;
 }
 
 export const SongDetails: FC<Props> = ({ song }) => {
+  const { back } = useRouter();
   return (
     <Grid>
-      <Grid item>Title: {song.title}</Grid>
+      <Grid item xs={12}>
+        <Button sx={{ my: 1 }} onClick={back} variant="outlined" startIcon={<ArrowBackIcon />}>
+          Back
+        </Button>
+      </Grid>
+      <Grid item>
+        <Typography variant="h3">{song.title}</Typography>
+      </Grid>
     </Grid>
   );
 };
