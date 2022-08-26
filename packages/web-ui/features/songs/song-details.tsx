bf90d946c7bb770ba2a9cf4186a890cb2f94fff8
StanislavMakhnytskyi <makhnitskiy.stanislav@gmail.com>
2022-08-26 18:27:35 +0400

    feature(web-ui): prettify grid for song details

--- packages/web-ui/features/songs/song-details.tsx
+++ packages/web-ui/features/songs/song-details.tsx
@@ -1,5 +1,6 @@
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import { useRouter } from 'next/router';
@@ -17,19 +18,21 @@ export const SongDetails: FC<Props> = ({ song }) => {
   const { back } = useRouter();
 
   return (
-    <Grid>
+    <Grid container>
       <Grid item xs={12}>
         <Button sx={{ my: 1 }} onClick={back} variant="outlined" startIcon={<ArrowBackIcon />}>
           Back
         </Button>
       </Grid>
-      <Grid item>
+      <Grid item xs={12}>
         <Typography variant="h3">{song.title}</Typography>
       </Grid>
-      <Grid item>
+      <Grid item lg={4} xs={12} wrap="nowrap">
         <Lyrics lyrics={song.lyrics} />
       </Grid>
-      <Grid item>
+      <Grid item lg={8} />
+      <Grid item lg={4}>
+        <Divider sx={{ mt: 3 }} />
         <CreateLyrics />
       </Grid>
     </Grid>
