2b05372bafccf607b4eb50f2c9283f8b74debddd
StanislavMakhnytskyi <makhnitskiy.stanislav@gmail.com>
2022-08-26 18:46:15 +0400

    feature(web-ui): add thumb up button to lyric

--- packages/web-ui/features/lyrics/lyrics.tsx
+++ packages/web-ui/features/lyrics/lyrics.tsx
@@ -1,3 +1,5 @@
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import IconButton from '@mui/material/IconButton';
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
@@ -15,10 +17,13 @@ export const Lyrics: FC<Props> = ({ lyrics }) => {
   return (
     <Stack spacing={1}>
       {lyrics.map((lyric) => (
-        <Paper key={lyric.id} sx={{ p: 2 }}>
+        <Paper key={lyric.id} sx={{ p: 2, display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
           <Typography sx={{ display: 'flex' }} variant="h5">
             {lyric.content}
           </Typography>
+          <IconButton aria-label="thumb-up">
+            <ThumbUpIcon />
+          </IconButton>
         </Paper>
       ))}
     </Stack>
