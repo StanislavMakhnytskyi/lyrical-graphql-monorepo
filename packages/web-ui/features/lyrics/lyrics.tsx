152b1fdb7bb5e26e8909a0485aed6a0a6b23b982
StanislavMakhnytskyi <makhnitskiy.stanislav@gmail.com>
2022-08-26 18:28:29 +0400

    feature(web-ui): prettify grid for lyrics

--- packages/web-ui/features/lyrics/lyrics.tsx
+++ packages/web-ui/features/lyrics/lyrics.tsx
@@ -1,3 +1,5 @@
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import React, { FC } from 'react';
 
@@ -11,13 +13,15 @@ export const Lyrics: FC<Props> = ({ lyrics }) => {
   if (!lyrics) return null;
 
   return (
-    <>
+    <Stack spacing={1}>
       {lyrics.map((lyric) => (
-        <Typography sx={{ display: 'flex' }} key={lyric.id} variant="h5">
-          {lyric.content}
-        </Typography>
+        <Paper key={lyric.id} sx={{ p: 2 }}>
+          <Typography sx={{ display: 'flex' }} variant="h5">
+            {lyric.content}
+          </Typography>
+        </Paper>
       ))}
-    </>
+    </Stack>
   );
 };
 
